6cac681d033ea595b402b3ba52e4f17a97f2fa9f
Manuel Rodriguez <valle.mrv@gmail.com>
2017-06-18 01:14:36 +0200

    Add more, more, more features

--- server/themagicapi/views.py
+++ server/themagicapi/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 PATH_DBS = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PATH_DBS = os.path.join(PATH_DBS, "dbs/")
@@ -17,26 +18,32 @@ def index(request):
         return HttpResponse(json.dumps({"Error": "Este servidor solo acepta peticiones POST"}))
     response = {}
     data = json.loads(request.POST.get("data"))
+
     if "add" in data:
         post = data.get("add")
-        db = post.get("db")+".db"
-        tb = post.get("tb")
-        multiple = "rows" in post
-        if multiple:
-            rows = post.get("rows")
-            response['add'] = []
-            for row in rows:
-                response = actionAdd(response, row, db, tb, multiple)
-        else:
-            row = post.get("row")
-            response = actionAdd(response, row, db, tb, multiple)
+        if not "db" in post:
+            return HttpResponse(json.dumps({"Error": "No se sabe el nombre de la db. Inique una con la Key='db'"}))
+
+        db = post.get("db") if 'db' in post.get("db") else post.get("db")+".db"
+        for k, v in post.items():
+            if k == "db":
+                pass
+            elif k == "rows":
+                rows = post.get("rows")
+                response['add'] = []
+                for row in rows:
+                    for kr, vr in row.items():
+                        response = actionAdd(response, vr, db, kr, True)
+            else:
+                response = actionAdd(response, v, db, k, False)
+
     if "get" in request.POST:
         response = get(response, data.get("get"))
     if "rm" in request.POST:
         response = rm(response, data.get("rm"))
 
-
-    return send_response(HttpResponse(json.dumps(response)))
+    http = HttpResponse(json.dumps(response, ensure_ascii=False))
+    return send_response(http)
 
 @csrf_exempt
 def sendFiles(request):
@@ -49,21 +56,31 @@ def send_response(response):
     response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
     response["Access-Control-Max-Age"] = "1000"
     response["Access-Control-Allow-Headers"] = "*"
-    response["Content-Type"] = "application/json"
+    response["Content-Type"] = "application/json; charset=utf-8"
     return response
 
 
 def actionAdd(objResponse, row_req, db, tb, multiple):
     row, relations = modifyRow(row_req, db, tb)
-    row_send = row.toJSON()
+    row.save()
+    row_send = row.toDICT()
     for relation in relations:
-        nameKey = relation["relationName"]
+        nameKey = relation["fieldName"] if 'fieldName' in relation else relation["relationName"]
         if not nameKey in row_send:
-            row_send[mameKey] = []
-        row_send[name]
-        getattr(row, mameKey).add(modifyRow(row_req[nameKey]))
+            row_send[nameKey] = []
+        for r in row_req[nameKey]:
+            if relation["relationTipo"] == "MANY":
+                child, relchild = modifyRow(r, db, nameKey, relationship={
+                    'relationName': tb,
+                    'relationTipo': "ONE",
+                })
+            else:
+                tbName = relation["relationName"]
+                child, relchild = modifyRow(r[tbName], db, tbName)
+                child.save()
 
-    row = row.toJSON()
+            getattr(row, nameKey).add(child)
+            row_send[nameKey].append(child.toDICT())
 
     if multiple:
         objResponse["add"].append(row_send)
@@ -73,36 +90,36 @@ def actionAdd(objResponse, row_req, db, tb, multiple):
 
 def repare_model(model, row, db, tb):
     for key, v in row.items():
-        search = filter(lambda field: field['fieldName'] == key, model["fields"])
-        if len(search) <= 0:
-            default, tipo = getTipo(v)
-            field = {
-                'fieldName': key,
-                'fieldDato': default,
-                'fieldTipo': tipo
-            }
-            model['fields'].append(field)
-            Models.alter(path=PATH_DBS, dbName=db, tableName=tb, field=field)
+        if not type(v) is list:
+            search = filter(lambda field: field['fieldName'] == key, model["fields"])
+            if len(search) <= 0:
+                default, tipo = getTipo(v)
+                field = {
+                    'fieldName': key,
+                    'fieldDato': default,
+                    'fieldTipo': tipo
+                }
+                model['fields'].append(field)
+                Models.alter(path=PATH_DBS, dbName=db, tableName=tb, field=field)
 
     return model
-
-
 
 def create_model(row):
     model = {"fields":[], "relationship": []}
-    for key, v in row.items():
-        default, tipo = getTipo(v)
 
-        model["fields"].append({
-         'fieldName': key,
-         'fieldDato': default,
-         'fieldTipo': tipo
-        })
+    for key, v in row.items():
+        if not type(v) is list:
+            default, tipo = getTipo(v)
+            model["fields"].append({
+             'fieldName': key,
+             'fieldDato': default,
+             'fieldTipo': tipo
+            })
 
     return model
 
 
-def modifyRow(row_json, db, tb):
+def modifyRow(row_json, db, tb, relationship=None):
     model = {}
     row = None
     if "ID" in row_json:
@@ -115,28 +132,36 @@ def modifyRow(row_json, db, tb):
                                  db=db, tb=tb)
         else:
             model = create_model(row_json)
+            if relationship:
+                model["relationship"].append(relationship)
 
         row = Models(path=PATH_DBS, dbName=db, tableName=tb, model=model)
     relations = []
     for key, v in row_json.items():
-        typeField = type(getattr(row, key))
-        if typeField is dict:
-            relationship = {
-                'relationName': key,
-                'relationTipo': "ONE",
+        if type(row_json[key]) is list:
+            fieldName = key
+            relationName = key
+            child = row_json[key]
+            tipo = "MANY"
+            for kr, vr in  child[0].items():
+                if type(vr) is dict:
+                    tipo = "MANYTOMANY"
+                    relationName = kr
+                    break;
+                else:
+                    break;
+
+            rship = {
+                'fieldName': fieldName,
+                'relationName': relationName,
+                'relationTipo': tipo,
             }
-            relations.append(relationship)
-        elif typeField is list:
-            relationship = {
-                'relationName': key,
-                'relationTipo': "MANY",
-            }
-            relations.append(relationship)
+            relations.append(rship)
         else:
             setattr(row, key, v)
 
-    row.save()
-    row.appendRelations(relations)
+    if len(relations) > 0:
+        row.appendRelations(relations)
     return row, relations
 
 def getTipo(val):
--- test/add_tests.py
+++ test/add_tests.py
@@ -1,57 +1,163 @@
+# -*- coding: utf-8 -*-
 import requests
+import sys
+# sys.setdefaultencoding() does not exist, here!
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
 
-params = {
+paramsRows = {
     'add':{
         "db":"valleorm",
-        "tb":"user",
         "rows":[{
-            "nombre": "Rafael",
-            "apellido": "Rodriguez",
-            "telf": "6666",
-            "precio": 1233.0,
-            "casa": True,
-            "perro": "pdae"
-            },{
-                "nombre": "Aitor",
-                "apellido": "Rodriguez",
-                "telf": "6666",
-                "precio": 1123.0,
-                "casa": True,
-                "perro": "pdae"
+               "user":{
+                    "nombre": "Rafael",
+                    "apellido": "ñññ¢¢¢",
+                    "telf": "6666",
+                    "precio": 1233.0,
+                    "casa": True,
+                    "perro": "pdae"
+                   }
+               },{
+                "user":{
+                    "nombre": "Aitor",
+                    "apellido": "Rodriguez",
+                    "telf": "6666",
+                    "precio": 1123.0,
+                    "casa": True,
+                    "perro": "pdae"
+                    }
                 },{
+                "user":{
                     "nombre": "Raul",
                     "apellido": "Rodriguez",
                     "telf": "6666",
                     "precio": 103.0,
                     "casa": True,
                     "perro": "pdae"
-                    }]
+                    }
+                }]
+        }
+}
+
+paramsRow = {
+    'add':{
+        "db":"valleorm.db",
+        'user':{
+            "nombre": "Pepito",
+            "apellido": "Lopez",
+            "telf": "6666",
+            }
+        }
+}
+
+paramsNewChild = {
+    'add':{
+        "db":"valleorm",
+        "user":{
+            "nombre": "Loco pitres",
+            "apellido": "Rodriguez",
+            "telf": "6666",
+            "precio": 1233.0,
+            "casa": True,
+            "perro": "pdae",
+            "direcciones":[{
+                "tipo":"calle",
+                "nombre": "MARACENA",
+                "localidad": "granada",
+                "codigo": '18012'
+               }]
+            }
         }
 }
 
 paramsChild = {
     'add':{
-        "db":"valleorm",
-        "tb":"user",
-        "row":{
-            "ID": 1,
+        "db":"valleorm.db",
+        "user":{
+            "ID": 3,
             "direcciones":[{
                 "tipo":"avd",
                 "nombre": "Francisco Ayala",
-                "localidad": "granada"
+                "localidad": "granada",
+                "codigo": '18012'
                }]
             },
         }
 }
 
+paramsManyToMany = {
+    'add': {
+        'db': 'valleorm',
+        'user':{
+            "nombre": "Loco pitres",
+            "apellido": "Rodriguez",
+            "telf": "6666",
+            "precio": 1233.0,
+            "casa": True,
+            "perro": "pdae",
+            "amigos":[
+               { "user":{
+                   "nombre": "amigito feliz",
+                   "apellido": "Rodriguez",
+               }}
+            ]
+        }
+    }
+}
 
 
-'''
+paramsManyToManyID = {
+    'add': {
+        'db': 'valleorm',
+        'user':{
+            'ID': 1,
+            'amigos':[
+               {
+                 "user":{
+                     'nombre': 'cocoroco',
+                     'apellido': 'kikiriki',
+                     'apodo': 'fernadiño'
+                 }
+               }
+            ]
+        }
+    }
+}
+
+
+paramsMultiple = {
+    'add':{
+        'db': 'valleorm',
+        'rows':[
+           {
+            'user': {
+                'nombre': 'Manolo',
+                'apellido': 'Rodriguez'
+             }
+           },
+           {
+            'user':{
+                'nombre': 'Alvaro',
+                'apellido': 'Rodriguez'
+             }
+           },
+           {
+              'user':{
+                'ID': 1,
+                'hermanos':[
+                        {
+                      'user':{
+                            'ID': 2
+                            }
+                        }
+                    ]
+              }
+
+           }
+        ]
+    }
+}
+
 import json
-r = requests.post("http://localhost:8000", {"data":json.dumps(paramsChild)})
+r = requests.post("http://localhost:8000", {"data":json.dumps(paramsMultiple)})
 print r.json()
-'''
-hola = [1,2,3]
-adios = [4,5,6]
-hola.append(adios)
-print hola
